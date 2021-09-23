a201b56ad4d82455e3b126c7a033a12f5807cb77
prodriguezolsavsky <81335679+prodriguezolsavsky@users.noreply.github.com>
2021-09-23 00:11:13 -0300

    Ejercicio 1 COMPLETO

--- TP4_Grupo_8/src/principal/VentanEj1.java
+++ TP4_Grupo_8/src/principal/VentanEj1.java
@@ -1,5 +1,9 @@
 package principal;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,48 +29,94 @@ public class VentanEj1 extends JFrame{
 		setSize(500, 500);
 		setLocationRelativeTo(null);
 		setTitle("Ejercicio 1");
-		getContentPane().setLayout(null);
 		
 		lblNombre = new JLabel();
-		lblNombre.setText("Nombre:");
 		lblNombre.setBounds(50, 50, 140, 30);
+		lblNombre.setText("Nombre:");
 		
 		lblApellido = new JLabel();
-		lblApellido.setText("Apellido:");
 		lblApellido.setBounds(50, 100, 140, 30);
+		lblApellido.setText("Apellido:");
 		
 		lblTelefono = new JLabel();
-		lblTelefono.setText("Telefono:");
 		lblTelefono.setBounds(50, 150, 140, 30);
+		lblTelefono.setText("Telefono:");
 		
 		lblFechaNac = new JLabel();
-		lblFechaNac.setText("Fecha de nacimiento:");
 		lblFechaNac.setBounds(50, 200, 140, 30);
+		lblFechaNac.setText("Fecha de nacimiento:");
 		
 		lblDatosIngresados = new JLabel();
-		lblDatosIngresados.setText("Los datos ingresados fueron: ");
 		lblDatosIngresados.setBounds(40, 300, 390, 30);
+		lblDatosIngresados.setText("Los datos ingresados fueron: ");
+		
 		
 		txtNombre = new JTextField();
-		txtNombre.setColumns(10);
 		txtNombre.setBounds(230, 50, 200, 36);
+		txtNombre.setColumns(10);
 		
 		txtApellido = new JTextField();
-		txtApellido.setColumns(10);
 		txtApellido.setBounds(230, 100, 200, 36);
+		txtApellido.setColumns(10);
 		
 		txtTelefono = new JTextField();
-		txtTelefono.setColumns(10);
 		txtTelefono.setBounds(230, 150, 200, 36);
+		txtTelefono.setColumns(10);
 		
 		txtFechaNac = new JTextField();
-		txtFechaNac.setColumns(10);
 		txtFechaNac.setBounds(230, 200, 200, 36);
+		txtFechaNac.setColumns(10);
 		
 		btnMostrar = new JButton();
-		btnMostrar.setText("Ejercicio 1");
 		btnMostrar.setBounds(290, 250, 140, 30);
-		//btnMostrar.addActionListener(new eventoBoton(0));
+		btnMostrar.setText("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public boolean validator() {
+				int cont = 0;
+				
+				if(txtNombre.getText().isEmpty()) {
+					cont+=1;
+					txtNombre.setBackground(Color.red);
+				}
+				
+				if(txtApellido.getText().isEmpty()) {
+					cont+=1;
+					txtApellido.setBackground(Color.red);
+				}
+				if(txtTelefono.getText().isEmpty()) {
+					cont+=1;
+					txtTelefono.setBackground(Color.red);
+				}
+				if(txtFechaNac.getText().isEmpty()) {
+					cont+=1;
+					txtFechaNac.setBackground(Color.red);	
+				}
+				
+				if (cont>0) {
+					return false;
+				}
+				else {
+					txtNombre.setBackground(Color.white);
+					txtApellido.setBackground(Color.white);
+					txtTelefono.setBackground(Color.white);
+					txtFechaNac.setBackground(Color.white);
+				}
+				return true;
+			}
+			
+			public void actionPerformed(ActionEvent e) {
+				if(validator()) {
+					lblDatosIngresados.setText("Los datos ingresados fueron: " + " " + txtNombre.getText() + " " +  txtApellido.getText() + " " + txtTelefono.getText() + " " + txtFechaNac.getText());
+					txtNombre.setText("");
+					txtApellido.setText("");
+					txtTelefono.setText("");
+					txtFechaNac.setText("");
+				}
+			}
+		});
+		
+		
+		getContentPane().setLayout(null);
 		
 		getContentPane().add(lblNombre);
 		getContentPane().add(lblApellido);
@@ -78,10 +128,14 @@ public class VentanEj1 extends JFrame{
 		getContentPane().add(txtTelefono);
 		getContentPane().add(txtFechaNac);
 		getContentPane().add(btnMostrar);
+		
 	}
 	
 	public void cambiarVisibilidad(boolean estado) {
 		setVisible(estado);
 	}
 	
+	
 }
+
+
