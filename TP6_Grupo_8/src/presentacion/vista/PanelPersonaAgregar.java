f1a752fe9a039d25675d601de676f866c88a05b4
spuyelli <spuyelli@gmail.com>
2021-10-18 17:57:16 -0300

    .

--- TP6_Grupo_8/src/presentacion/vista/PanelPersonaAgregar.java
+++ TP6_Grupo_8/src/presentacion/vista/PanelPersonaAgregar.java
@@ -2,14 +2,10 @@ package presentacion.vista;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
 public class PanelPersonaAgregar extends JPanel{
 	
